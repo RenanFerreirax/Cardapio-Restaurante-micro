934e5375f5c620660fcf3ef54790c48c93224191
Renan <renanturmaensinomedio.com@gmail.com>
2026-07-07 09:39:45 -0300

    Add authentication middleware to restaurant routes

--- src/routes/restaurantes.routes.js
+++ src/routes/restaurantes.routes.js
@@ -1,11 +1,12 @@
 const restaurantesController = require('../controllers/restaurantes.controller');
+const { authenticateToken } = require("./middlewares/authenticateToken");
 
 module.exports = (server) => {
   // Rotas para Restaurantes
-  server.get('/restaurantes', restaurantesController.listarTodos);
-  server.get('/restaurantes/:id', restaurantesController.buscarPorId);
-  server.post('/restaurantes', restaurantesController.criar);
-  server.put('/restaurantes/:id', restaurantesController.atualizar);
-  server.patch('/restaurantes/:id', restaurantesController.atualizarParcial);
-  server.del('/restaurantes/:id', restaurantesController.excluir);
+  server.get('/restaurantes', authenticateToken, restaurantesController.listarTodos);
+  server.get('/restaurantes/:id', authenticateToken, restaurantesController.buscarPorId);
+  server.post('/restaurantes', authenticateToken, restaurantesController.criar);
+  server.put('/restaurantes/:id', authenticateToken, restaurantesController.atualizar);
+  server.patch('/restaurantes/:id', authenticateToken, restaurantesController.atualizarParcial);
+  server.del('/restaurantes/:id', authenticateToken, restaurantesController.excluir);
 };
